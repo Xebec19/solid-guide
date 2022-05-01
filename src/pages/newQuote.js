e578996e76e1b8a5c002b3ffdde26777218f4f72
xebec19 <rohant946@gmail.com>
2022-05-01 19:34:04 +0530

    added progress bar and http hook

--- src/pages/newQuote.js
+++ src/pages/newQuote.js
@@ -1,12 +1,24 @@
+import { useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 import QuoteForm from '../components/quotes/QuoteForm';
+import useHttp from '../hooks/use-http';
+import { addQuote } from '../lib/api';
+
 const NewQuote = () => {
+    const { sendRequest, status } = useHttp(addQuote);
     const history = useHistory();
+    useEffect(() => {
+        if(status === 'completed'){
+            history.push('/quotes');
+        }
+    }, [status]);
     history.pushState('/quotes');
     const addQuoteHandler = quoteData => {
         console.log(quoteData);
+        sendRequest(quoteData);
+        history.push('/quotes');
     }
-    return <QuoteForm onAddQuote={addQuoteHandler} />
+    return <QuoteForm isLoading={ isLoading } onAddQuote={addQuoteHandler} />
 };
 
 export default NewQuote;
